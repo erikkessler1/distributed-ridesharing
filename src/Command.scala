ee3cea84e323a4d80d8315c3da9eae12dd8b8db7
Erik Kessker <erik.kessler1@gmail.com>
2016-05-20 13:17:39 -0400

    Cleanup Command

--- src/Command.scala
+++ src/Command.scala
@@ -1,5 +1,6 @@
 /**
  * Provides a way to issue commands to a World.
+ * Makes creating new commands easy - extend WorldCommand and implement execute.
  *
  * Erik Kessler and Kevin Persons
  */
@@ -13,17 +14,14 @@ object Command {
   /**
    * Execute command, op, with args on the world
    */
-  def execute(op: String, args: List[String]) =
-    // Find and execute the command that matches
-    commands.find(_.c == op) match {
-      case Some(command) => command.execute(args)
-      case None          => "Command not understood"
-    }
-
+  def execute(op: String, args: List[String]) = commands.find(_.c == op) match {
+                                                  case Some(command) => command.execute(args)
+                                                  case None          => "Command not understood"
+                                                }
 }
 
 /**
- * Class for encapulating a command. C is the command, args are the arguement
+ * Class for encapulating a command. c is the command, args are the arguement
  * options, and description describes it.
  *
  * Concrete classes implement the execute method which defines whath the command
@@ -43,17 +41,22 @@ abstract class WorldCommand(val c: String, val args: String, val description: St
 class StepCommand() extends WorldCommand("s", "[n] [d]", "Step simulation n steps with d delay.") {
 
   override def execute(args: List[String]) = {
+
+    // Get the number of steps to move
     val n = args match {
       case n::as => Util.toInt(n).getOrElse(1)
       case Nil   => 1
     }
 
+    // Get the delay between steps
     val delay = args match {
       case _::d::as => Util.toInt(d).getOrElse(200)
       case _        => 200
     }
 
+    // Execute the step
     World.step(n, delay)
+    
     "Step"
   }
 }
@@ -70,6 +73,7 @@ class FocusCommand() extends WorldCommand("f", "[n]", "Set focus on peer n.") {
       case Nil   => None
     }
 
+    // Set the focus on that peer or report an error
     n match {
       case Some(n) => World.setFocus(n); s"Focus set to peer $n"
       case None    => "Peer not found"
@@ -77,6 +81,10 @@ class FocusCommand() extends WorldCommand("f", "[n]", "Set focus on peer n.") {
   }
 }
 
+/**
+ * Command that prints the total number of network messages sent since
+ * the start of the simulation.
+ */
 class MessagesCommand() extends WorldCommand("m", "", "Print total messages sent.") {
 
   override def execute(args: List[String]) = {
@@ -84,6 +92,10 @@ class MessagesCommand() extends WorldCommand("m", "", "Print total messages sent
   }
 }
 
+/**
+ * Command that prints the number of matched rides over the total number
+ * of rides requested since the start of the simulation.
+ */
 class RidesCommand() extends WorldCommand("r", "", "Print ratio of ride matching.") {
 
   override def execute(args: List[String]) = {
