30ad399caf430db626873af1bb1c61e286523a9a
Erik Kessker <erik.kessler1@gmail.com>
2016-05-20 16:31:41 -0400

    Stuff

--- src/Network.scala
+++ src/Network.scala
@@ -12,15 +12,29 @@ object Network {
    * Issues a ride request from the sender to the reciever.
    */
   def sendRequest(sender: Peer, reciever: Peer) : Boolean = {
+    // Increment by two because we send data both ways
     sentMessages += 2
+
+    // See how the reciever responds and update rideMatches appropriately
     val matched = reciever.respondToRequest(sender)
     if (matched) rideMatches += 1
+    
     return matched
   }
 
+  /**
+   * Sends an update about location.
+   * Sender sends its updated positions and the receiver responds with its peer list.
+   */
   def sendUpdate(sender: Peer, reciever: Peer): (List[FrozenPeer], Int) = {
+    // Communication is two-way
+    sentMesages += 2
+
+    // Send the update to the reciever
     reciever.getUpdate(sender)
-    (reciever.getPeerLocs.map(p => new FrozenPeer(p.id, p.pos, p.ts, p.peer)).filter(_.id != sender.id), reciever.pos)
+
+    // Send the reciever's peer list back to the initial sender
+    (reciever.getPeerLocs.map(p => new FrozenPeer(p.id, p.pos, p.ts, p.peer)).filterNot(_.id == sender.id), reciever.pos)
   }
 
 }
